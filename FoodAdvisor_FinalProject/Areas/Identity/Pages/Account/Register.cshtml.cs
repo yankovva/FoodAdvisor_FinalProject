2fc42b9206b69e945d09adacaad5a214ccf53fe0
yankovva <maggie.yankovaaa@gmail.com>
2024-10-31 18:44:39 +0200

    Changing the cshtml files and setting application user emal to be the email they regestired with.

--- FoodAdvisor_FinalProject/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ FoodAdvisor_FinalProject/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -104,9 +104,10 @@ namespace FoodAdvisor_FinalProject.Areas.Identity.Pages.Account
             if (ModelState.IsValid)
             {
                 var user = CreateUser();
+                user.Email = Input.Email;
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
-              //  await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+                //await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)
