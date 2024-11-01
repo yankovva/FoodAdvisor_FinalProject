ea2bbdd64818d72b19e4a72874dc651b24fb2555
yankovva <maggie.yankovaaa@gmail.com>
2024-11-01 12:22:44 +0200

    Adding Users favourites .

---
+++ FoodAdvisor.ViewModels/FavouritesViewModel/FavouritesIndexViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodAdvisor.ViewModels.FavouritesViewModel
+{
+    public class FavouritesIndexViewModel
+    {
+        public required string Id { get; set; }
+        public required string Name { get; set; }
+        public required string Description { get; set; }
+        public required string ImageUrl { get; set; }
+        public required string Category { get; set; }
+
+
+    }
+}
---
+++ FoodAdvisor_FinalProject/Controllers/FavouritesController.cs
@@ -0,0 +1,57 @@
+﻿using FoodAdvisor.Data;
+using FoodAdvisor.Data.Models;
+using FoodAdvisor.ViewModels.FavouritesViewModel;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FoodAdvisor_FinalProject.Controllers
+{
+	[Authorize]
+	public class FavouritesController : BaseController
+	{
+        private readonly FoodAdvisorDbContext dbContext;
+        private readonly UserManager<ApplicationUser> userManager;
+
+
+        public FavouritesController(FoodAdvisorDbContext dbContext, UserManager<ApplicationUser> userManager)
+        {
+            this.dbContext = dbContext;
+            this.userManager = userManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            string? userId = userManager.GetUserId(User)!;
+
+            //if (string.IsNullOrEmpty(userId))
+            //{
+            //    return RedirectToRoute("/Identity/Account/Login");
+            //}
+
+            IEnumerable<FavouritesIndexViewModel> favourites = await this.dbContext
+                .UsersRestaurants
+                .Include(ur => ur.Restaurant)
+                .Where(ur => ur.Restaurant.IsDeleted == false && ur.ApplicationUserId.ToString().ToLower() == userId.ToLower())
+                .Select(ur => new FavouritesIndexViewModel()
+                {
+                    Id = ur.ApplicationUserId.ToString(),
+                    Name = ur.Restaurant.Name,
+                    Description = ur.Restaurant.Description,
+                    Category = ur.Restaurant.Category.Name,
+                    ImageUrl = ur.Restaurant.ImageURL ?? string.Empty
+                })
+                .ToArrayAsync();
+                
+
+            return View(favourites);
+        }
+        //public async Task<IActionResult> AddToFavourites()
+        //{
+
+        //}
+
+    }
+}
