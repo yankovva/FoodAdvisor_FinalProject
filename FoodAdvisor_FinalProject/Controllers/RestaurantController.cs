cdaa3840f45871eb0f0a1788d20b0c484a085a0d
yankovva <maggie.yankovaaa@gmail.com>
2024-11-01 15:47:07 +0200

    Adding the Edit Restaurant Funcitonality and fixing some small errors.

--- FoodAdvisor_FinalProject/Controllers/RestaurantController.cs
+++ FoodAdvisor_FinalProject/Controllers/RestaurantController.cs
@@ -30,7 +30,8 @@ namespace FoodAdvisor_FinalProject.Controllers
                     Name = p.Name,
                     Description = p.Description,
                     ImageURL = p.ImageURL,
-                    Category = p.Category.Name
+                    Category = p.Category.Name,
+					Publisher = p.Publisher.UserName ?? string.Empty
                 })
                 .AsNoTracking()
                 .ToArrayAsync();
@@ -188,6 +189,44 @@ namespace FoodAdvisor_FinalProject.Controllers
 			return View(model);
 		}
 
+		[HttpPost]
+		public async Task<IActionResult> Edit(RestaurantAddViewModel model, string id)
+		{
+			if (ModelState.IsValid == false)
+			{
+				model.Categories = await GetCategories();
+				model.Cities = await GetCities();
+				return View(model);
+			}
+			Guid restaurantGuid = Guid.Empty;
+			bool isGuidValid = this.IsGuidValid(id, ref restaurantGuid);
+			if (!isGuidValid)
+			{
+				//if the Guid(Id) is not valid, redirecting to index page
+				return this.RedirectToAction(nameof(Index));
+			}
+
+			Restaurant? editedRestaurant = await this.dbContext
+				.Restaurants
+				.FindAsync(restaurantGuid);
+
+			if (editedRestaurant == null && editedRestaurant.IsDeleted == true)
+			{
+				throw new ArgumentException("Invalid ID");
+			}
+
+			editedRestaurant.Name = model.Name;
+			editedRestaurant.Address = model.Address;
+			editedRestaurant.ImageURL = model.ImageURL;
+			editedRestaurant.CategoryId = model.CategoryId;
+			editedRestaurant.PublisherId = Guid.Parse(GetCurrentUserId()) ;
+			editedRestaurant.Description = model.Description;
+			editedRestaurant.CityId = model.CityId;
+
+			await this.dbContext.SaveChangesAsync();
+
+			return RedirectToAction(nameof(Index));
+		}
 
 		private string? GetCurrentUserId()
 		{
