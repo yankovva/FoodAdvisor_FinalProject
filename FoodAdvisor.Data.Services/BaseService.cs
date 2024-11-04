c9706c919623ee461b882cdab99d8dc08fac60a6
yankovva <maggie.yankovaaa@gmail.com>
2024-11-04 12:53:26 +0200

    Adding IBaseService interface and BaseService class witch will checkif the id is valid.

---
+++ FoodAdvisor.Data.Services/BaseService.cs
@@ -0,0 +1,30 @@
+﻿using FoodAdvisor.Data.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodAdvisor.Data.Services
+{
+	public class BaseService : IBaseService
+	{
+		public bool IsGuidValid(string? id, ref Guid parsedGuid)
+		{
+			// Non-existing parameter in the URL
+			if (String.IsNullOrWhiteSpace(id))
+			{
+				return false;
+			}
+
+			// Invalid parameter in the URL
+			bool isGuidValid = Guid.TryParse(id, out parsedGuid);
+			if (!isGuidValid)
+			{
+				return false;
+			}
+
+			return true;
+		}
+	}
+}
---
+++ FoodAdvisor.Data.Services/Interfaces/IBaseService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodAdvisor.Data.Services.Interfaces
+{
+	public interface IBaseService
+	{
+		bool IsGuidValid(string? id, ref Guid parsedGuid);
+	}
+}
