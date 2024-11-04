b90f7433fbd10a36ea51632faccfae9a4d3d521e
yankovva <maggie.yankovaaa@gmail.com>
2024-11-04 12:49:59 +0200

    Adding Repository Class.

--- FoodAdvisor.Data/Repository/IRepository.cs
+++ FoodAdvisor.Data/Repository/IRepository.cs
@@ -14,12 +14,13 @@ namespace FoodAdvisor.Data.Repository
 
 		IEnumerable<TType> GetAll();
 		Task<IEnumerable<TType>> GetAllAsync();
+		IEnumerable<TType> GetAllAttached();
 
 		void Add(TType item);
 		Task AddAsync(TType item);
 
-		bool Delete(TType item);
-		Task<bool> DeleteAsync(TType item);
+		bool Delete(TId id);
+		Task<bool> DeleteAsync(TId id);
 
 		bool Update(TType item);
 		Task<bool> UpdateAsync(TType item);
---
+++ FoodAdvisor.Data/Repository/Repository.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace FoodAdvisor.Data.Repository
+{
+	public class Repository<TType, TId> : IRepository<TType, TId> where TType : class
+	{
+		private readonly FoodAdvisorDbContext dbContext;
+		private readonly DbSet<TType> dbSet;
+		public Repository(FoodAdvisorDbContext dbContext, DbSet<TType> dbSet)
+		{
+			this.dbContext = dbContext;
+			this.dbSet = this.dbContext.Set<TType>();
+
+		}
+		public void Add(TType item)
+		{
+			dbSet.Add(item);
+			dbContext.SaveChanges();
+		}
+
+		public async Task AddAsync(TType item)
+		{
+			dbSet.Add(item);
+			await dbContext.SaveChangesAsync();
+		}
+
+		public bool Delete(TId id)
+		{
+			TType entity = GetById(id);
+			if (entity == null)
+			{
+				return false;
+			}
+			dbSet.Remove(entity);
+			dbContext.SaveChanges();
+
+			return true;
+		}
+
+		public async Task<bool> DeleteAsync(TId id)
+		{
+			TType entity = await GetByIdAsync(id);
+			if (entity == null)
+			{
+				return false;
+			}
+			dbSet.Remove(entity);
+			await dbContext.SaveChangesAsync();
+
+			return true;
+		}
+
+		public IEnumerable<TType> GetAll()
+		{
+			return dbSet.ToArray();
+		}
+
+		public async Task<IEnumerable<TType>> GetAllAsync()
+		{
+			return await dbSet.ToArrayAsync();
+		}
+
+		public IEnumerable<TType> GetAllAttached()
+		{
+			return dbSet.AsQueryable();
+		}
+
+		public TType GetById(TId id)
+		{
+			TType? entity = this.dbSet
+			   .Find(id);
+
+			return entity;
+		}
+
+		public async Task<TType> GetByIdAsync(TId id)
+		{
+			TType? entity = await this.dbSet
+						   .FindAsync(id);
+
+			return entity;
+		}
+
+		public bool Update(TType item)
+		{
+			try
+			{
+				dbSet.Attach(item);
+				dbContext.Entry(item)
+					.State = EntityState.Modified;
+				dbContext.SaveChanges();
+				return true;
+			}
+			catch (Exception e)
+			{
+				return false;
+			}
+		}
+
+		public async Task<bool> UpdateAsync(TType item)
+		{
+			try
+			{
+				dbSet.Attach(item);
+				dbContext.Entry(item)
+					.State = EntityState.Modified;
+				await dbContext.SaveChangesAsync();
+				return true;
+			}
+			catch (Exception e)
+			{
+				return false;
+			}
+		}
+	}
+}
