7e572cd58767ad728b12bcbc0ec98211c8aadf3c
yankovva <maggie.yankovaaa@gmail.com>
2024-11-04 12:29:16 +0200

    Adding IRepository Interface.

---
+++ FoodAdvisor.Data/Repository/IRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Formats.Tar;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodAdvisor.Data.Repository
+{
+	public interface IRepository<TType,TId>
+	{
+		TType GetById(TId id);
+		Task<TType> GetByIdAsync(TId id);
+
+		IEnumerable<TType> GetAll();
+		Task<IEnumerable<TType>> GetAllAsync();
+
+		void Add(TType item);
+		Task AddAsync(TType item);
+
+		bool Delete(TType item);
+		Task<bool> DeleteAsync(TType item);
+
+		bool Update(TType item);
+		Task<bool> UpdateAsync(TType item);
+		
+	}
+}
