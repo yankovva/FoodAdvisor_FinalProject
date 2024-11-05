5475bee90a46ebaf618b1b0e8090d227b059add3
yankovva <maggie.yankovaaa@gmail.com>
2024-11-05 15:12:39 +0200

    Adding Add comment .

---
+++ FoodAdvisor.ViewModels/CommentViewModel/AddCommentViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using static FoodAdvisor.Common.EntityValidationConstants.RestaurantComment;
+
+namespace FoodAdvisor.ViewModels.CommentViewModel
+{
+	public class AddCommentViewModel
+	{
+		[Required]
+		[MaxLength(MessageMaxLenght)]
+		[MinLength(MessageMinLenght)]
+		public string Message { get; set; } = null!;
+		
+		
+	}
+}
--- FoodAdvisor_FinalProject/Controllers/RestaurantCommentController.cs
+++ FoodAdvisor_FinalProject/Controllers/RestaurantCommentController.cs
@@ -1,12 +1,53 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FoodAdvisor.Data;
+using FoodAdvisor.Data.Models;
+using FoodAdvisor.ViewModels.CommentViewModel;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FoodAdvisor_FinalProject.Controllers
 {
-    public class RestaurantCommentController : Controller
-    {
-        public IActionResult Add()
-        {
-            return View();
-        }
-    }
+	public class RestaurantCommentController : BaseController
+	{
+		private readonly FoodAdvisorDbContext dbContext;
+
+		public RestaurantCommentController(FoodAdvisorDbContext _dbContext)
+		{
+			this.dbContext = _dbContext;
+
+		}
+		[HttpGet]
+		public async Task<IActionResult> Add()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Add(AddCommentViewModel model, string id)
+		{
+
+			if (!this.ModelState.IsValid)
+			{
+				return this.View(model);
+			}
+
+			Guid restaurantGuid = Guid.Empty;
+			bool isGuidValid = this.IsGuidValid(id, ref restaurantGuid);
+			if (!isGuidValid)
+			{
+				return this.RedirectToAction(nameof(Index));
+			}
+
+			RestaurantComment comment = new RestaurantComment()
+			{
+				Message = model.Message,
+				UserId = Guid.Parse(GetCurrentUserId()),
+				RestaurantId = restaurantGuid,
+				CreatedDate = DateTime.Now
+			};
+
+			await this.dbContext.RestaurantsComments.AddAsync(comment);
+			await this.dbContext.SaveChangesAsync();
+
+			return RedirectToAction("Details", "Restaurant");
+		}
+	}
 }
