315c249dfec7a8adcf17773411f3a230bd8c5378
yankovva <maggie.yankovaaa@gmail.com>
2024-11-08 13:02:55 +0200

    Adding details recepie vies model and view

---
+++ FoodAdvisor.ViewModels/RecepiesViewModels/DetailsRecepieViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodAdvisor.ViewModels.RecepiesViewModels
+{
+	public class DetailsRecepieViewModel
+	{
+        public Guid Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+		public DateTime CreatetOn { get; set; }
+        public string Publisher { get; set; } = null!;
+		public string ImageURL { get; set; } = null!;
+		public int CookingTime { get; set; }
+    }
+}
