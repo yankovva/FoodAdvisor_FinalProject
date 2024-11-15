76fb288c400761a7bf89dacb215964a529336bf8
yankovva <maggie.yankovaaa@gmail.com>
2024-11-15 17:14:14 +0200

    Updating Login view

--- FoodAdvisor_FinalProject/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ FoodAdvisor_FinalProject/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -2,23 +2,16 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 #nullable disable
 
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
+using FoodAdvisor.Data.Models;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
-using FoodAdvisor.Data.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace FoodAdvisor_FinalProject.Areas.Identity.Pages.Account
 {
-    public class LoginModel : PageModel
+	public class LoginModel : PageModel
     {
         private readonly SignInManager<ApplicationUser> _signInManager;
         private readonly UserManager<ApplicationUser> _userManager;
