d009c03024f96e5ae86c332820913ac241bc4755
yankovva <maggie.yankovaaa@gmail.com>
2024-11-29 11:42:18 +0200

    Adding admin controller and IndexAllUsersViewModel

---
+++ FoodAdvisor.ViewModels/UserViewModels/IndexAllUsersViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodAdvisor.ViewModels.UserViewModels
+{
+    public class IndexAllUsersViewModel
+    {
+        public string Id { get; set; } = null!;
+
+        public string? Email { get; set; }
+        public string? Username { get; set; }
+
+        public IEnumerable<string> CurrentRoles { get; set; } = null!;
+
+    }
+}
---
+++ FoodAdvisor_FinalProject/Areas/Admin/Controllers/AdminManageController.cs
@@ -0,0 +1,27 @@
+﻿using FoodAdvisor.Data.Services.Interfaces;
+using FoodAdvisor_FinalProject.Controllers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using static FoodAdvisor.Common.ApplicationConstants;
+
+namespace FoodAdvisor_FinalProject.Areas.Admin.Controllers
+{
+    [Area(AdminRoleName)]
+    [Authorize(Roles = AdminRoleName)]
+    public class AdminManageController : BaseController
+    {
+        private readonly IUserService userService;
+        public AdminManageController(IUserService userService)
+        {
+            this.userService = userService;
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var users = await this.userService.GetAllUsersAsync();
+            return this.View(users);
+        }
+    }
+}
