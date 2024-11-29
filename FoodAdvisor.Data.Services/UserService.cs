c81e7c51dc3ed899f59dd8df1cafee546418b993
yankovva <maggie.yankovaaa@gmail.com>
2024-11-29 15:41:11 +0200

    Implementing the new methods

--- FoodAdvisor.Data.Services/UserService.cs
+++ FoodAdvisor.Data.Services/UserService.cs
@@ -1,4 +1,5 @@
 ﻿using FoodAdvisor.Data.Models;
+using FoodAdvisor.Data.Repository.Interfaces;
 using FoodAdvisor.Data.Services.Interfaces;
 using FoodAdvisor.ViewModels.UserViewModels;
 using Microsoft.AspNetCore.Identity;
@@ -15,32 +16,150 @@ namespace FoodAdvisor.Data.Services
     {
         private readonly UserManager<ApplicationUser> userManager;
         private readonly RoleManager<IdentityRole<Guid>> roleManager;
+        private readonly IManagerService managerService;
 
         public UserService(UserManager<ApplicationUser> userManager,
-            RoleManager<IdentityRole<Guid>> roleManager)
+            RoleManager<IdentityRole<Guid>> roleManager,
+            IManagerService managerService)
         {
             this.userManager = userManager;
             this.roleManager = roleManager;
+            this.managerService = managerService;
         }
+
+        public async Task<bool> DeleteUserAsync(Guid userId)
+        {
+            ApplicationUser? user = await userManager
+               .FindByIdAsync(userId.ToString());
+
+            if (user == null)
+            {
+                return false;
+            }
+
+            IdentityResult? result = await this.userManager
+                .DeleteAsync(user);
+            if (!result.Succeeded)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
         public async Task<IEnumerable<IndexAllUsersViewModel>> GetAllUsersAsync()
         {
             IEnumerable<ApplicationUser> allUsers = await this.userManager.Users
                .ToArrayAsync();
-
+          
             ICollection<IndexAllUsersViewModel> allUsersViewModel = new List<IndexAllUsersViewModel>();
 
             foreach (ApplicationUser user in allUsers)
             {
+                var userId = user.Id.ToString();
+                bool isManager = await this.managerService.IsUserManagerAsync(userId);  
                 allUsersViewModel.Add(new IndexAllUsersViewModel()
                 {
-                    Id = user.Id.ToString(),
+                    Id = userId,
                     Email = user.Email,
                     Username = user.UserName,
-                    CurrentRoles = userManager.GetRolesAsync(user).Result.ToList()
+                    IsManager = isManager,
+                    CurrentRoles = userManager.GetRolesAsync(user).Result.ToList(),
                 });
             }
+         
 
             return allUsersViewModel;
         }
-    }
+
+        public async Task<bool> UpdateRoleAsync(Guid userId, string roleName)
+        {
+            ApplicationUser? user = await userManager
+                .FindByIdAsync(userId.ToString());
+
+            bool roleExists = await this.roleManager
+                .RoleExistsAsync(roleName);
+
+            if (user == null || roleExists == false)
+            {
+                return false;
+            }
+
+            bool userIsInRole = await this.userManager.IsInRoleAsync(user, roleName);
+            if (userIsInRole == false)
+            {
+                var result = await this.userManager
+                    .AddToRoleAsync(user, roleName);
+
+                if (result.Succeeded == false)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public async Task<bool> RemoveRoleAsync(Guid userId, string roleName)
+        {
+            ApplicationUser? user = await userManager
+                .FindByIdAsync(userId.ToString());
+
+            bool roleExists = await this.roleManager
+                .RoleExistsAsync(roleName);
+
+            if (user == null || roleExists == false)
+            {
+                return false;
+            }
+
+            bool userIsInTheRole = await this.userManager.IsInRoleAsync(user, roleName);
+            if (userIsInTheRole)
+            {
+                var result = await this.userManager
+                    .RemoveFromRoleAsync(user, roleName);
+
+                if (result.Succeeded == false)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public async Task<bool> MakeManagerAsync(Guid userId)
+        {
+            ApplicationUser? user = await userManager
+                .FindByIdAsync(userId.ToString());
+
+            bool alreadyManager = await this.managerService.IsUserManagerAsync(userId.ToString());
+            if ( user==null || alreadyManager == true )
+            {
+                return false;
+            }
+
+            await this.managerService.AddManagerAsync(userId, "", "");
+            return true;
+		}
+
+		public async Task<bool> RemoveManagerAsync(Guid userId)
+        {
+			ApplicationUser? user = await userManager
+				.FindByIdAsync(userId.ToString());
+
+			bool isManager = await this.managerService.IsUserManagerAsync(userId.ToString());
+			if (user == null || isManager == false)
+			{
+				return false;
+			}
+
+            bool isDeleted = await this.managerService.RemoveManagerAsync(userId);
+            if (isDeleted == false)
+            {
+                return false;
+            }
+            return true;
+		}
+
+	}
 }
