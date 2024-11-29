da77cbc0066ed7b91666e710b6f192e7661e6322
yankovva <maggie.yankovaaa@gmail.com>
2024-11-29 15:41:30 +0200

    Updating AdminController

--- FoodAdvisor_FinalProject/Areas/Admin/Controllers/AdminManageController.cs
+++ FoodAdvisor_FinalProject/Areas/Admin/Controllers/AdminManageController.cs
@@ -3,6 +3,7 @@ using FoodAdvisor_FinalProject.Controllers;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using static FoodAdvisor.Common.ApplicationConstants;
+using static FoodAdvisor.Common.EntityValidationConstants;
 
 namespace FoodAdvisor_FinalProject.Areas.Admin.Controllers
 {
@@ -23,5 +24,95 @@ namespace FoodAdvisor_FinalProject.Areas.Admin.Controllers
             var users = await this.userService.GetAllUsersAsync();
             return this.View(users);
         }
-    }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateRole(string userId, string roleName)
+        {
+            Guid userGuid = Guid.Empty;
+            if (!this.IsGuidValid(userId, ref userGuid))
+            {
+                return this.RedirectToAction(nameof(Index));
+            }
+
+            var isUpdated = await this.userService.UpdateRoleAsync(userGuid, roleName);
+            if (isUpdated == false)
+            {
+                return this.RedirectToAction(nameof(Index), "Home");
+            }
+
+            return this.RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RemoveRole (string userId, string roleName)
+        {
+            Guid userGuid = Guid.Empty;
+            if (!this.IsGuidValid(userId, ref userGuid))
+            {
+                return this.RedirectToAction(nameof(Index));
+            }
+
+            bool isRemoved = await this.userService.RemoveRoleAsync(userGuid, roleName);
+            if (isRemoved == false)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            return RedirectToAction(nameof(Index));
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(string userId)
+        {
+            Guid userGuid = Guid.Empty;
+            if (!this.IsGuidValid(userId, ref userGuid))
+            {
+                return this.RedirectToAction(nameof(Index));
+            }
+            bool isDelted = await this.userService.DeleteUserAsync(userGuid);
+            if (isDelted == false)
+            {
+                return this.RedirectToAction(nameof(Index));
+            }
+
+            return this.RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> MakeManager(string userId)
+        {
+            Guid userGuid = Guid.Empty;
+            if (!this.IsGuidValid(userId, ref userGuid))
+            {
+                return this.RedirectToAction(nameof(Index));
+            }
+            bool isMadeManager = await this.userService
+                .MakeManagerAsync(userGuid);
+
+            if (isMadeManager == false)
+            {
+                return this.RedirectToAction(nameof(Index));
+            }
+
+            return this.RedirectToAction(nameof(Index));
+        }
+		[HttpPost]
+		public async Task<IActionResult> RemoveManager(string userId)
+		{
+			Guid userGuid = Guid.Empty;
+			if (!this.IsGuidValid(userId, ref userGuid))
+			{
+				return this.RedirectToAction(nameof(Index));
+			}
+			bool isRemoved = await this.userService
+				.RemoveManagerAsync(userGuid);
+
+			if (isRemoved == false)
+			{
+				return this.RedirectToAction(nameof(Index));
+			}
+
+			return this.RedirectToAction(nameof(Index));
+		}
+	}
 }
