e961dc21d2100e4eb016754a880412869fce59b3
yankovva <maggie.yankovaaa@gmail.com>
2024-12-03 17:38:26 +0200

    Adding new ManagerFormViewModel and fifxing the controller add manager logic

---
+++ FoodAdvisor.ViewModels/UserViewModels/ManagerFormViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodAdvisor.ViewModels.UserViewModels
+{
+    public class ManagerFormViewModel
+    {
+		public string UserId { get; set; } = null!;
+
+		[Required]
+		public string PhoneNumber { get; set; } = null!;
+
+		[Required]
+		public string Address { get; set; } = null!;
+	}
+}
--- FoodAdvisor_FinalProject/Areas/Admin/Controllers/AdminManageController.cs
+++ FoodAdvisor_FinalProject/Areas/Admin/Controllers/AdminManageController.cs
@@ -1,101 +1,89 @@
-﻿using FoodAdvisor.Data.Services.Interfaces;
+﻿using FoodAdvisor.Data.Models;
+using FoodAdvisor.Data.Services.Interfaces;
+using FoodAdvisor.ViewModels.UserViewModels;
 using FoodAdvisor_FinalProject.Controllers;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using static FoodAdvisor.Common.ApplicationConstants;
-using static FoodAdvisor.Common.EntityValidationConstants;
 
 namespace FoodAdvisor_FinalProject.Areas.Admin.Controllers
 {
-    [Area(AdminRoleName)]
-    [Authorize(Roles = AdminRoleName)]
-    public class AdminManageController : BaseController
-    {
-        private readonly IUserService userService;
-        public AdminManageController(IUserService userService)
-        {
-            this.userService = userService;
+	[Area(AdminRoleName)]
+	[Authorize(Roles = AdminRoleName)]
+	public class AdminManageController : BaseController
+	{
+		private readonly IUserService userService;
+		private readonly UserManager<ApplicationUser> userManager;
+		private readonly IManagerService managerService;
+		public AdminManageController(IUserService userService, UserManager<ApplicationUser> userManager, IManagerService managerService)
+		{
+			this.userService = userService;
+			this.userManager = userManager;
+			this.managerService = managerService;
+		}
 
-        }
+		[HttpGet]
+		public async Task<IActionResult> Index()
+		{
+			var users = await this.userService.GetAllUsersAsync();
+			return this.View(users);
+		}
 
-        [HttpGet]
-        public async Task<IActionResult> Index()
-        {
-            var users = await this.userService.GetAllUsersAsync();
-            return this.View(users);
-        }
+		[HttpPost]
+		public async Task<IActionResult> UpdateRole(string userId, string roleName)
+		{
+			Guid userGuid = Guid.Empty;
+			if (!this.IsGuidValid(userId, ref userGuid))
+			{
+				return this.RedirectToAction(nameof(Index));
+			}
 
-        [HttpPost]
-        public async Task<IActionResult> UpdateRole(string userId, string roleName)
-        {
-            Guid userGuid = Guid.Empty;
-            if (!this.IsGuidValid(userId, ref userGuid))
-            {
-                return this.RedirectToAction(nameof(Index));
-            }
+			var isUpdated = await this.userService.UpdateRoleAsync(userGuid, roleName);
+			if (isUpdated == false)
+			{
+				return this.RedirectToAction(nameof(Index), "Home");
+			}
 
-            var isUpdated = await this.userService.UpdateRoleAsync(userGuid, roleName);
-            if (isUpdated == false)
-            {
-                return this.RedirectToAction(nameof(Index), "Home");
-            }
+			return this.RedirectToAction(nameof(Index));
+		}
 
-            return this.RedirectToAction(nameof(Index));
-        }
+		[HttpPost]
+		public async Task<IActionResult> RemoveRole(string userId, string roleName)
+		{
+			Guid userGuid = Guid.Empty;
+			if (!this.IsGuidValid(userId, ref userGuid))
+			{
+				return this.RedirectToAction(nameof(Index));
+			}
 
-        [HttpPost]
-        public async Task<IActionResult> RemoveRole (string userId, string roleName)
-        {
-            Guid userGuid = Guid.Empty;
-            if (!this.IsGuidValid(userId, ref userGuid))
-            {
-                return this.RedirectToAction(nameof(Index));
-            }
+			bool isRemoved = await this.userService.RemoveRoleAsync(userGuid, roleName);
+			if (isRemoved == false)
+			{
+				return RedirectToAction(nameof(Index));
+			}
+			return RedirectToAction(nameof(Index));
 
-            bool isRemoved = await this.userService.RemoveRoleAsync(userGuid, roleName);
-            if (isRemoved == false)
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            return RedirectToAction(nameof(Index));
+		}
 
-        }
+		[HttpPost]
+		public async Task<IActionResult> Delete(string userId)
+		{
+			Guid userGuid = Guid.Empty;
+			if (!this.IsGuidValid(userId, ref userGuid))
+			{
+				return this.RedirectToAction(nameof(Index));
+			}
+			bool isDelted = await this.userService.DeleteUserAsync(userGuid);
+			if (isDelted == false)
+			{
+				return this.RedirectToAction(nameof(Index));
+			}
 
-        [HttpPost]
-        public async Task<IActionResult> Delete(string userId)
-        {
-            Guid userGuid = Guid.Empty;
-            if (!this.IsGuidValid(userId, ref userGuid))
-            {
-                return this.RedirectToAction(nameof(Index));
-            }
-            bool isDelted = await this.userService.DeleteUserAsync(userGuid);
-            if (isDelted == false)
-            {
-                return this.RedirectToAction(nameof(Index));
-            }
+			return this.RedirectToAction(nameof(Index));
+		}
 
-            return this.RedirectToAction(nameof(Index));
-        }
 
-        [HttpPost]
-        public async Task<IActionResult> MakeManager(string userId)
-        {
-            Guid userGuid = Guid.Empty;
-            if (!this.IsGuidValid(userId, ref userGuid))
-            {
-                return this.RedirectToAction(nameof(Index));
-            }
-            bool isMadeManager = await this.userService
-                .MakeManagerAsync(userGuid);
-
-            if (isMadeManager == false)
-            {
-                return this.RedirectToAction(nameof(Index));
-            }
-
-            return this.RedirectToAction(nameof(Index));
-        }
 		[HttpPost]
 		public async Task<IActionResult> RemoveManager(string userId)
 		{
@@ -114,5 +102,33 @@ namespace FoodAdvisor_FinalProject.Areas.Admin.Controllers
 
 			return this.RedirectToAction(nameof(Index));
 		}
+
+
+		[HttpPost]
+		public async Task<IActionResult> CreateManager([FromForm] ManagerFormViewModel model)
+		{
+			if (!ModelState.IsValid)
+			{
+				return Json(new
+				{
+					success = false,
+					errors = ModelState.Values
+					.SelectMany(v => v.Errors.Select(e => e.ErrorMessage))
+				});
+			}
+			bool result = await this.userService.MakeManagerAsync(model.UserId, model.PhoneNumber, model.Address);
+
+			if (result == false)
+			{
+				return Json(new
+				{
+					success = false,
+					errors = ModelState.Values
+					.SelectMany(v => v.Errors.Select(e => e.ErrorMessage))
+				});
+			}
+			return Json(new { success = true });
+		}
 	}
 }
+
